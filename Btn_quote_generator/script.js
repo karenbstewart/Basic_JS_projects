c073647f3e3fb5ee383227ef6eb49c889843de87
8e6 <kastewart2002@googlemail.com>
2020-06-12 14:13:19 +0100

    first commit

---
+++ Btn_quote_generator/script.js
@@ -0,0 +1,28 @@
+const quotes = [
+{
+	name:'Stephen King',
+	quote:'Get busy living or get busy dying.'
+},
+{
+	name:'John F. Kennedy',
+	quote:'Those who dare to fail miserably can achieve greatly'
+},
+{
+	name:'Abraham Lincoln',
+	quote:'I\'m a success today because i had a friend who believed in me and i didn\'t have the heart to let him down.'
+},
+{
+	name:'Leonardo Da Vinci ',
+	quote:'It had long since come to my attention that people of accomplishment rarely sat back and let things happen to them. They went out and happened to things'
+},
+{
+	name:'Leo Tolstoy',
+	quote:'If you want to be happy, BE! '
+},
+];
+
+
+
+
+
+
